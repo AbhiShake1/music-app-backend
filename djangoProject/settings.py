5e9d8db8e5dcaeb5c016e5e0b3fedafb108d2aa0
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2022-03-04 16:20:42 +0545

    fix static_root config

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -120,4 +120,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'djangoProject')
