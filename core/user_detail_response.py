13aad9f737e452be244874b21490bf31114995b0
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2022-03-07 21:59:23 +0545

    update result map

--- core/user_detail_response.py
+++ core/user_detail_response.py
@@ -5,11 +5,11 @@ from django.http import HttpResponse
 def get_user_detail_response(user: User) -> HttpResponse:
     import json
     data = {
-        'username': user.username,
+        'userName': user.username,
         'email': user.email,
-        'first_name': user.first_name,
-        'last_name': user.last_name,
-        'date_joined': str(user.date_joined),
-        'is_active': user.is_active,
+        'firstName': user.first_name,
+        'lastName': user.last_name,
+        'dateJoined': str(user.date_joined),
+        'isActive': user.is_active,
     }
     return HttpResponse(json.dumps(data))
