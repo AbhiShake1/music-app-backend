feb877d1647c4730cb9ba2f759381d3c6d14aece
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2022-04-10 08:54:47 +0545

    add feedback model

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import User
-from django.db import models
+from django.core.validators import MaxValueValidator
+from django.db.models import *
+
 
 # Create your models here.
-
+class Post(Model):
+    username = ForeignKey(User, on_delete=CASCADE)
+    comment = TextField()
+    rating = PositiveIntegerField(validators=[MaxValueValidator(5, message='Rating cannot go beyond 5')])
--- feedbacks/views.py
+++ feedbacks/views.py
@@ -1,6 +1,9 @@
 import pusher
+from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+
+from feedbacks.models import Post
 
 pusher_client = pusher.Pusher(
     app_id='1363891',
@@ -14,30 +17,32 @@ pusher_client = pusher.Pusher(
 # Create your views here.
 @csrf_exempt
 def post_detail(request: HttpRequest) -> HttpResponse:
-    from messages_repo import MessagesRepo
-
-    repo = MessagesRepo()
-
     if request.method == 'POST':
         import json
         post_data: dict = json.loads(request.body.decode())
-        post_id = post_data['post_id']
         comment = post_data['comment']
-        username = post_data['username']
+        username = User.objects.get(username=post_data['username'])
         rating = post_data['rating']
-        message = repo.create(post_id, username, comment, int(rating))
-        pusher_client.trigger(post_id, 'feedback', message)
-        data = {'post_id': post_id,
-                'comment': comment,
-                }
+        message = Post.objects.create(username=username, comment=comment, rating=int(rating))
+        data = {
+            'post_id': message.id,
+            'comment': comment,
+        }
         return HttpResponse(json.dumps(data), headers={
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": True,
         })
     if request.method == 'GET':
         import json
-        messages = repo.get_all()
-        return HttpResponse(json.dumps(messages), headers={
+        messages = Post.objects.all()
+        result: dict = {}
+        for message in messages:
+            key = str(message.username)
+            if key in result:
+                result[key] += message.comment
+            else:
+                result[key] = [message.comment]
+        return HttpResponse(json.dumps(result, default=str), headers={
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": True,
         })
