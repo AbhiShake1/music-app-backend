6eaefb49df268f172ceaa62fde7ec6ac2d414fda
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2022-04-18 13:19:25 +0545

    test return post from request

--- music_upload/views.py
+++ music_upload/views.py
@@ -12,7 +12,7 @@ from music_upload.models import Music
 def upload_music(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST' and request.FILES['music']:
         import json
-        return HttpResponse(json.loads(request.body.decode()))
+        return HttpResponse(request.POST)
         post_data = json.loads(request.body.decode())
         try:
             title = post_data['title']
