8d1ebbed09b0d96edf1a10bed4c73db820900f7d
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2022-04-18 13:23:04 +0545

    use post again

--- music_upload/views.py
+++ music_upload/views.py
@@ -12,8 +12,7 @@ from music_upload.models import Music
 def upload_music(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST' and request.FILES['music']:
         import json
-        return HttpResponse(request.POST)
-        post_data = json.loads(request.body.decode())
+        post_data = request.POST
         try:
             title = post_data['title']
         except:
